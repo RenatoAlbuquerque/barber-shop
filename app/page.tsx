2d014a85ab6f6ae2c3578a2d89401986987f9a7c
Renato Albuquerque <renatoabreukz@gmail.com>
2024-08-19 21:52:59 -0300

    refactor: update in metadata site and change favicon

--- app/page.tsx
+++ app/page.tsx
@@ -48,7 +48,7 @@ const Home = async () => {
       <div className="p-5">
         {/* TEXTO */}
         <h2 className="text-xl font-bold">
-          Olá, {session?.user ? session.user.name : "bem vindo"}!
+          Olá, {session?.user ? session.user.name : "Bem vindo"}!
         </h2>
         <p>
           <span className="capitalize">
